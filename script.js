4fc27c4b78cd4d640e694bba259b79fc07ae42d5
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2024-02-10 11:09:30 -0600

    Next Previous & Progress

--- script.js
+++ script.js
@@ -43,6 +43,48 @@ function pauseSong() {
   audio.pause();
 }
 
+// Previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+//Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+//Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
 // Event listeners
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
@@ -52,3 +94,16 @@ playBtn.addEventListener('click', () => {
     playSong();
   }
 });
+
+//Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song Update
+audio.addEventListener('timeupdate', updateProgress);
+
+//Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+//Sound ends
+audio.addEventListener('ended', nextSong);
